bbb91060e4ee67eb9e01a2389187b3173520fb35
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-17 14:18:22 +0300

    Product Repository

---
+++ App.Repositories/Products/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Repositories.Products
+{
+    public class ProductRepository(AppDbContext context) : GenericRepository<Product>(context), IProductRepository
+    {
+        public Task<List<Product>> GetTopPriceProductsAsync(int count)
+        {
+            return _context.Products.OrderByDescending(p => p.Price).Take(count).ToListAsync(); // Already returns Task<List<Product>>
+        }
+    }
+}
