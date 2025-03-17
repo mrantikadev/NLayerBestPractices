40fb1863a12493581bc7526e2674ecbbda74effd
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-17 14:34:37 +0300

    UnitOfWork

--- App.Repositories/Extensions/RepositoryExtensions.cs
+++ App.Repositories/Extensions/RepositoryExtensions.cs
@@ -22,6 +22,8 @@ namespace App.Repositories.Extensions
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             return services;
         }
     }
--- App.Services/Products/ProductService.cs
+++ App.Services/Products/ProductService.cs
@@ -4,5 +4,9 @@ namespace App.Services.Products
 {
     public class ProductService(IProductRepository productRepository) : IProductService
     {
+        public Task<List<Product>> GetTopPriceProductsAsync(int count)
+        {
+            return productRepository.GetTopPriceProductsAsync(count);
+        }
     }
 }
