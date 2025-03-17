468e01329b794857c8d630a9216195325eb98c78
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-17 14:44:08 +0300

    Result Pattern

--- App.Services/Products/ProductService.cs
+++ App.Services/Products/ProductService.cs
@@ -4,9 +4,31 @@ namespace App.Services.Products
 {
     public class ProductService(IProductRepository productRepository) : IProductService
     {
-        public Task<List<Product>> GetTopPriceProductsAsync(int count)
+        public async Task<ServiceResult<List<Product>>> GetTopPriceProductsAsync(int count)
         {
-            return productRepository.GetTopPriceProductsAsync(count);
+            var products = await productRepository.GetTopPriceProductsAsync(count);
+            return new ServiceResult<List<Product>>
+            {
+                Data = products
+            };
+        }
+
+        public async Task<ServiceResult<Product>> GetProductByIdAsync(int id)
+        {
+            var product = await productRepository.GetByIdAsync(id);
+
+            if (product is null)
+            {
+                return new ServiceResult<Product>
+                {
+                    ErrorMessage = new List<string>() { "Product not found." }
+                };
+            }
+
+            return new ServiceResult<Product>
+            {
+                Data = product
+            };
         }
     }
 }
---
+++ App.Services/ServiceResult.cs
@@ -0,0 +1,11 @@
+﻿namespace App.Services
+{
+    public class ServiceResult<T>
+    {
+        public T? Data { get; set; }
+        public List<string>? ErrorMessage { get; set; }
+        public bool IsSuccess => ErrorMessage == null || ErrorMessage.Count == 0;
+        public bool IsFailure => !IsSuccess;
+
+    }
+}
