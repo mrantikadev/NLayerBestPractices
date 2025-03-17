3018e7aa1089eed5913e499c065d63658466728c
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-17 11:33:33 +0300

    Repositories 1

--- App.API/Program.cs
+++ App.API/Program.cs
@@ -1,3 +1,6 @@
+using App.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetSection(ConnectionStringOption.Key).Get<ConnectionStringOption>();
+
+    options.UseSqlServer(connectionString!.SqlServer);
+});
 
 var app = builder.Build();
 
