7ddc5f3b6ad6869d20f3f84c033c09b3d980121f
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-18 16:19:11 +0300

    ExceptionHandler

---
+++ App.Services/ExceptionHandlers/CriticalException.cs
@@ -0,0 +1,4 @@
+﻿namespace App.Services.ExceptionHandlers
+{
+    public class CriticalException : Exception;
+}
---
+++ App.Services/ExceptionHandlers/CriticalExceptionHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace App.Services.ExceptionHandlers
+{
+    public class CriticalExceptionHandler : IExceptionHandler
+    {
+        public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            if (exception is CriticalException)
+                Console.WriteLine("Critical Exception Occurred");
+
+            return ValueTask.FromResult(false);
+        }
+    }
+}
