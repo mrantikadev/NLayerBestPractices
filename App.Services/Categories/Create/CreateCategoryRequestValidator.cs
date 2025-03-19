1d2782d9a6cdb54f4f9aab38c92a88d69349700e
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-19 15:02:22 +0300

    Category Create and Update Validators

---
+++ App.Services/Categories/Create/CreateCategoryRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace App.Services.Categories.Create
+{
+    public class CreateCategoryRequestValidator : AbstractValidator<CreateCategoryRequest>
+    {
+        public CreateCategoryRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .Length(3, 20).WithMessage("Name must be between 3 and 20 characters.");
+        }
+    }
+}
---
+++ App.Services/Categories/Update/UpdateCategoryRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace App.Services.Categories.Update
+{
+    public class UpdateCategoryRequestValidator : AbstractValidator<UpdateCategoryRequest>
+    {
+        public UpdateCategoryRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .Length(3, 20).WithMessage("Name must be between 3 and 20 characters.");
+        }
+    }
+}
